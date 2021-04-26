1f1ab87aa32d7de74659ffcf05126c1edae65531
Julia Shevchuk <julia.shevchuk01@gmail.com>
2021-04-26 21:09:55 +0200

    translated comments, added MainPage.dox

--- src/poly.c
+++ src/poly.c
@@ -44,7 +44,7 @@ static Poly PolyInit(size_t size) {
 
 Poly PolyClone(const Poly *p) {
     if (PolyIsCoeff(p)) {
-        return *p; // A copy of *p will be returned here
+        return *p; //tutaj zostanie zwrócona kopia *p
     } else {
         Poly new_poly = PolyInit(p->size);
         for (size_t i = 0; i < p->size; ++i) {
@@ -58,7 +58,7 @@ Poly PolyClone(const Poly *p) {
  * Porównuje dwa jenomiany według wykładników.
  * @param[in] lhs : wskaźnik na jednomian
  * @param[in] rhs : wskaźnik na jednomian
- * @return wynik porównania,
+ * @return wynik porównania:
  *        - dodatnia wartość całkowita, jeśli pierwszy argument jest większy niż drugi,
  *        - ujemna wartość całkowita, jeśli pierwszy argument jest mniejszy niż drugi,
  *        - zero, jeśli argumenty są równoważne
@@ -117,34 +117,36 @@ Poly PolyAddMonos(size_t count, const Mono monos[]) {
     if (count == 0) {
         return PolyZero();
     }
-    /*We don't own the monos array, only its contents. So we need copy of the array*/
+    /* Przejmujemy na własność zawartość tablicy monos(NIE TABLICĘ!).
+     * Więc potrzebujemy kopii tablicy*/
     Mono *monos_copy = (Mono *) malloc(sizeof(struct Mono) * count);
     if (monos_copy == NULL) {
         exit(ERROR_EXIT_STATUS);
     }
     memcpy(monos_copy, monos, sizeof(struct Mono) * count);
 
-    /* From this moment all monos in poly will be sorted by exp. Functions like multiplication
-     * that break the order will be required to restore it*/
+    /* Od tego momentu wszystkie jednomiany w wielomianie będą sortowane według wykładnika.
+     * Funkcje takie jak mnożenie, które psują porządek, będą musiały go przywrócić*/
     qsort(monos_copy, count, sizeof(struct Mono), MonoCompare);
 
-    /*From this moments for each exp there is only one Mono in Poly with such exp*/
+    /*Od tego momentu dla każdego wykładnika istnieje dokładnie jeden jednomian w wielomianie z takim wykładnikiem*/
     size_t current = 0;
     for (size_t next = 1; next < count; ++next) {
-        if (MonoGetExp(&monos_copy[next]) != MonoGetExp(&monos_copy[current])) { //new unique exp
+        if (MonoGetExp(&monos_copy[next]) != MonoGetExp(&monos_copy[current])) { //nowy unikalny wykładnik
             if (MonoIsZero(&monos_copy[current])) {
-                monos_copy[current] = monos_copy[next]; //skip zero(replace with new mono)
+                monos_copy[current] = monos_copy[next]; //pomijamy zero (zamieniamy na nowy jednomian)
             } else {
-                monos_copy[++current] = monos_copy[next]; //add new mono
+                monos_copy[++current] = monos_copy[next]; //dodaj nowy jednomian
             }
         } else {
             Mono sum = MonoAdd(&monos_copy[current], &monos_copy[next]);
             MonoDestroy(&monos_copy[current]);
             MonoDestroy(&monos_copy[next]);
-            monos_copy[current] = sum; //zero may appear, we'll get rid of it in next iteration
+            monos_copy[current] = sum; //może pojawić się zero, pozbędziemy się go w następnej iteracji
         }
     }
-    if (current != 0 && MonoIsZero(&monos_copy[current])) { //we need to get rid of last zero. If last zero is the only element we leave it
+    /*Musimy pozbyć się ostatniego zera. Jeśli ostatnie zero jest jedynym elementem zostawiamy go*/
+    if (current != 0 && MonoIsZero(&monos_copy[current])) {
         current--;
     }
 
@@ -157,7 +159,7 @@ Poly PolyAddMonos(size_t count, const Mono monos[]) {
  * Zwraca większy z dwóch wykładników.
  * @param[in] lhs : wykładnik @f$lhs@f$
  * @param[in] rhs : wykładnik @f$rhs@f$
- * @return największy wykładnik z @p lhs i @p rhs. Jeśli oba są równoważne, zwracane jest @p rhs.
+ * @return największy wykładnik z @f$lhs@f$ i @f$rhs@f$. Jeśli oba są równoważne, zwracane jest @f$rhs@f$.
  */
 static poly_exp_t max(poly_exp_t lhs, poly_exp_t rhs) {
     return (lhs > rhs) ? lhs : rhs;
@@ -178,7 +180,9 @@ poly_exp_t PolyDegBy(const Poly *p, size_t var_idx) {
         return PolyIsZero(p) ? -1 : 0;
     }
     if (var_idx == 0) {
-        return MonoGetExp(&p->arr[p->size - 1]);//array of mono is sorted(creation of poly from mono array), last elt has largest exp
+        /* tablica jednomianów jest posortowana(tworzenie wielomianu z tablicy jednomianów),
+         * ostatni element ma największy wykładnik*/
+        return MonoGetExp(&p->arr[p->size - 1]);
     } else {
         poly_exp_t result = EXP_MIN;
         for (size_t i = 0; i < p->size; ++i) {
@@ -202,7 +206,7 @@ poly_exp_t PolyDeg(const Poly *p) {
 
 bool PolyIsEq(const Poly *p, const Poly *q) {
     if (PolyIsCoeff(p) == PolyIsCoeff(q)) {
-        if (PolyIsCoeff(p)) { //p and q are both coeffs
+        if (PolyIsCoeff(p)) { //p i q to oba współczynniki
             return p->coeff == q->coeff;
         } else if (p->size != q->size) {
             return false;
@@ -261,17 +265,17 @@ static Poly PolyMulCoef(const Poly *p, poly_coeff_t q) {
         } else {
             mul = PolyMulCoef(&p->arr[i].p, q);
         }
-        if (!PolyIsZero(&mul)) { //skip zero
+        if (!PolyIsZero(&mul)) { //pomijamy zero
             Mono mono = MonoFromPoly(&mul, MonoGetExp(&p->arr[i]));
             result.arr[result.size++] = mono;
         }
-        /* Note: we don't destroy anything here.
-         * 1. mono owns mul -> no need to destroy mul
-         * 2.1(mul isn't zero) result owns mono
-         * 2.2(mul is zero) -> no arrays -> nothing to destroy*/
+        /* Uwaga: tu nie używamy destroy
+         * 1. mono jest właścicielem mul -> nie używamy destroy dla mul
+         * 2.1(mul nie zero) result jest właścicielem mono
+         * 2.2(mul zero) -> nie ma tablicy jednomianów -> nie używamy destroy*/
     }
-    if (result.size == 0) { //there were only zeros, nothing was written to the result
-        free(result.arr); //we don't use destroy function, because result is uninitialized value
+    if (result.size == 0) { //były tylko zera, nic nie zostało zapisane do wyniku
+        free(result.arr); //nie używamy funkcji destroy, ponieważ wynik jest niezainicjalizowaną wartością
         return PolyZero();
     }
     PolyOptimize(&result);
@@ -294,7 +298,7 @@ static Poly PolyMulPoly(const Poly *p, const Poly *q) {
     for (size_t i = 0; i < p->size; ++i) {
         for (size_t j = 0; j < q->size; ++j) {
             Poly mul = PolyMul(&p->arr[i].p, &q->arr[j].p);
-            if (!PolyIsZero(&mul)) { //skip zero
+            if (!PolyIsZero(&mul)) { //pomijamy zero
                 init_arr[current] = MonoFromPoly(&mul, MonoGetExp(&p->arr[i]) + MonoGetExp(&q->arr[j]));
                 current++;
             }
@@ -304,7 +308,8 @@ static Poly PolyMulPoly(const Poly *p, const Poly *q) {
     if (current == 0) {
         result = PolyZero();
     } else {
-        result = PolyAddMonos(current, init_arr);//we use this function to restore broken order and uniqueness of exp
+        //używamy tej funkcji, aby przywrócić zepsuty porządek i unikalność każdego wykładnika
+        result = PolyAddMonos(current, init_arr);
     }
     free(init_arr);
     return result;
@@ -348,7 +353,8 @@ static Poly PolyAddCoef(const Poly *p, poly_coeff_t q) {
     }
     assert(p->size > 0);
     Poly result;
-    if (MonoGetExp(&p->arr[0]) == 0) { //exps are sorted and unique -> coef can only appear in first position
+    /*wykładniki są posortowane i unikalne -> współczynnik może pojawić się tylko na pierwszej pozycji*/
+    if (MonoGetExp(&p->arr[0]) == 0) {
         size_t current = 0;
 
         Poly sum;
@@ -362,7 +368,7 @@ static Poly PolyAddCoef(const Poly *p, poly_coeff_t q) {
             result = PolyInit(p->size);
             result.arr[current++] = mono;
         } else {
-            result = PolyInit(p->size - 1); //skip zero
+            result = PolyInit(p->size - 1); //pomijamy zero
         }
         for (size_t i = 1; i < p->size; ++i) {
             result.arr[current++] = MonoClone(&p->arr[i]);
@@ -406,28 +412,28 @@ static Poly PolyAddPoly(const Poly *p, const Poly *q) {
                 result.arr[current] = sum;
                 current++;
             }
-            /* Note: we don't destroy anything here.
-             * 1. mono owns sum -> no need to destroy sum
-             * 2.1(Mono isn't zero) result owns mono
-             * 2.2(Mono is zero) Mono's coef(Poly) is zero -> no arrays -> nothing to destroy*/
+            /* Uwaga: tu nie używamy destroy
+            * 1. mono jest właścicielem sum -> nie używamy destroy dla sum
+            * 2.1(Mono nie zero) result jest właścicielem mono
+            * 2.2(Mono zero)-> wielomian tego jednomianu zero -> nie ma tablicy jednomianów -> nie używamy destroy*/
             i++;
             j++;
         }
     }
-    /*Case i > p->size && j = q->size)*/
+    /*Przypadek i > p->size && j = q->size)*/
     while (i < p->size) {
         result.arr[current] = MonoClone(&p->arr[i]);
         i++;
         current++;
     }
-    /*Case i = p->size && j > q->size)*/
+    /*Przypadek i = p->size && j > q->size)*/
     while (j < q->size) {
         result.arr[current] = MonoClone(&q->arr[j]);
         j++;
         current++;
     }
-    if (current == 0) { //there were only zeros, nothing was written to the result
-        free(result.arr); //we don't use destroy function, because result is uninitialized value
+    if (current == 0) { //były tylko zera, nic nie zostało zapisane do wyniku
+        free(result.arr); //nie używamy funkcji destroy, ponieważ wynik jest niezainicjalizowaną wartością
         return PolyZero();
     }
     result.size = current;
@@ -457,17 +463,17 @@ Poly PolyAdd(const Poly *p, const Poly *q) {
  * @param[in] exp : wykładnik @f$exp@f$
  * @return @f$base^{exp}@f$
  */
-//Source: https://www.geeksforgeeks.org/modular-exponentiation-power-in-modular-arithmetic/
+//Źródło: https://www.geeksforgeeks.org/modular-exponentiation-power-in-modular-arithmetic/
 static poly_coeff_t Power(poly_coeff_t base, poly_exp_t exp) {
     poly_coeff_t result = 1;
     while (exp > 0) {
-        // If exp is odd, multiply base with result
+        //Jeśli exp jest nieparzysty, mnożymy base i result
         if ((exp & 1) != 0) {
             result *= base;
         }
-        // exp must be even now
+        //teraz exp musi być parzysty
         exp = exp >> 1; // exp = exp/2
-        base *= base;  // Change base to base^2
+        base *= base;
     }
     return result;
 }
@@ -476,9 +482,9 @@ Poly PolyAt(const Poly *p, poly_coeff_t x) {
     if (PolyIsCoeff(p)) {
         return PolyClone(p);
     }
-    /* Monos in poly are always sorted by exp. To avoid counting x^n for every mono in poly,
-     * we count x^n for current mono and difference m-n (next mono has greater exp m).
-     * So x^m = x^n * x^(m-n)  */
+    /* Jednomiany w wielomianie są zawsze sortowane według wykładników. Aby uniknąć liczenia x^n dla
+     * każdego jednomianu w wielomianie, liczymy x^n dla bieżącego jednomianu i różnicę m-n(następny
+     * jednomian ma większy wykładnik m). Więc x^m = x^n * x^(m-n)*/
     poly_coeff_t x_power_value = 1;
     poly_exp_t current_exp = 0;
     Poly result = PolyZero();
